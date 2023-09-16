178eb46efc607812b99e1d59871532eb5a58e3d2
Timo <tibue69@gmail.com>
2023-09-16 11:14:08 +0200

    Move bot class to main file

--- utils/__init__.py
+++ utils/__init__.py
@@ -1,12 +1,2 @@
 import utils.database as dab
 from config import *
-
-
-def convert_time(seconds: int) -> str:
-    if seconds < 60:
-        return f"{round(seconds)} seconds"
-    minutes = seconds / 60
-    if minutes < 60:
-        return f"{round(minutes)} minutes"
-    hours = minutes / 60
-    return f"{round(hours)} hours"
