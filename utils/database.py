8a999b75697f4b664cd77d39fcec5ab08b2624c6
Timo <tibue69@gmail.com>
2023-09-20 12:55:01 +0200

    Use context manager

--- utils/database.py
+++ utils/database.py
@@ -14,13 +14,12 @@ class UserDB(ezcord.DBHandler):
         )
 
     async def add_coins(self, user_id, amount):
-        transaction = self.start()
-        await transaction.exec("INSERT OR IGNORE INTO users (user_id) VALUES (?)", (user_id,))
-        await transaction.exec(
-            "UPDATE users SET coins = coins + ? WHERE user_id = ?",
-            (amount, user_id),
-        )
-        await transaction.close()
+        async with self.start() as cursor:
+            await cursor.exec("INSERT OR IGNORE INTO users (user_id) VALUES (?)", (user_id,))
+            await cursor.exec(
+                "UPDATE users SET coins = coins + ? WHERE user_id = ?",
+                (amount, user_id),
+            )
 
     async def get_users(self):
         return await self.all("SELECT user_id, coins FROM users")
